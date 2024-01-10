e49183f84c4de8b8e464aa05274752fa227f93b2
RaniffSilva <raniff.silva@iatecam.org.br>
2024-01-10 10:13:47 -0400

    Projeto-relatorio-upgrades

--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-report',
@@ -7,36 +7,24 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./report.component.scss']
 })
 export class ReportComponent implements OnInit {
-  reportForm: FormGroup = new FormGroup({
-    selectedShift: new FormControl('', Validators.required),
-    startDate: new FormControl('', Validators.required),
-    endDate: new FormControl('', Validators.required),
+  reportForm: FormGroup = this.fb.group({
+    selectedShift: ['', Validators.required],
+    startDate: ['', Validators.required],
+    endDate: ['', Validators.required],
   });
-  selectedShift: any;
-  startDate: string = '';
-  endDate: string = '';
+
   shiftOptions = [
     { label: 'Morning', value: 'morning' },
     { label: 'Afternoon', value: 'afternoon' },
     { label: 'Evening', value: 'evening' },
   ];
-
   currentDate: Date = new Date();
 
+  constructor(private fb: FormBuilder) {}
+
   ngOnInit() {
-    setInterval(() => {
-      this.currentDate = new Date();
-    }, 1000);
   }
 
   generateReport() {
-    if (this.reportForm.valid) {
-      console.log('Generating report...');
-      console.log('Shift:', this.selectedShift?.value);
-      console.log('Start Date:', this.startDate);
-      console.log('End Date:', this.endDate);
-    } else {
-      console.log('Form is invalid. Cannot generate report.');
-    }
   }
 }
