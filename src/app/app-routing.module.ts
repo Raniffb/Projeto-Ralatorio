b468dd3885f01ade174a0ceeb5768884f6c4f01d
Raniff Barroncas Silva <raniffbarroncas@gmail.com>
2025-01-26 13:29:51 -0300

    Fix: Header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ReportComponent } from './report/report.component';
 
 const routes: Routes = [
-  { path: 'report', component: ReportComponent },
+  { path: '', component: ReportComponent },
 ];
 
 @NgModule({
